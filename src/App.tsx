a12d864e96ed4d37b04b19c956860b5868df7511
Rajkishor Thakur <rajkishort596@gmail.com>
2025-10-18 19:08:23 +0530

    Add Hangman game components and word list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import words from "./wordList.json";
+import HangmanDrawing from "./HangmanDrawing";
+import Keyboard from "./Keyboard";
+import HangmanWord from "./HangmanWord";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
   return (
-      <h1>Hangman</h1>    
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>win or lose</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
